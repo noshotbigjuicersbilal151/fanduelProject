61e7c341e81feb0a306ee400e8b0ef9134f301ad
marinersjk00 <kuschnerj@gmail.com>
2022-02-23 19:48:57 -0800

    Added enter date function and incorporated it into main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+#include <Team.h>
+using namespace std;
+string enterDate(){
+	string date;
+	cout << "Please enter date in MMDDYYYY format: " << endl;
+	cin >> date;
+	
+return date;
+}
 
 int main(int argc, char* argv[])
 {
+cout << "Welcome to our Daily Fantasy Best Lineup Retriever Tool" << endl;
+
+String date = enterDate();
+
 return 0;
 }
