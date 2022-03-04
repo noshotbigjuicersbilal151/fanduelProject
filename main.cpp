4874125a2aad34e9ee8f966a11e12c2e175accad
marinersjk00 <kuschnerj@gmail.com>
2022-03-04 01:14:44 -0800

    wrote a main.cpp that can read csv

--- main.cpp
+++ main.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <Team.h>
+#include <vector>
+#include <sstream>
+ 
 using namespace std;
-string enterDate(){
-	string date;
-	cout << "Please enter date in MMDDYYYY format: " << endl;
-	cin >> date;
-	
-return date;
-}
-
-int main(int argc, char* argv[])
+ 
+vector<vector<string>> csvReader()
 {
-cout << "Welcome to our Daily Fantasy Best Lineup Retriever Tool" << endl;
-
-String date = enterDate();
-
-return 0;
+	string fname = "bestPlayers.csv";
+ 
+	vector<vector<string>> content;
+	vector<string> row;
+	string line, word;
+ 
+	fstream file (fname, ios::in);
+	if(file.is_open())
+	{
+		while(getline(file, line))
+		{
+			row.clear();
+ 
+			stringstream str(line);
+ 
+			while(getline(str, word, ','))
+				row.push_back(word);
+			content.push_back(row);
+		}
+	}
+	else
+		cout<<"Could not open the file\n";
+ 
+	return content;
+ 
 }
+ 
+int main(){
+
+vector<vector<string>> content = csvReader();
+for(int i=0;i<content.size();i++)
+	{
+		for(int j=0;j<content[i].size();j++)
+		{
+			cout<<content[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+return 0;
+} 
+ 
