910225a9c014a7ec8e2e20a44e43b9c9c980d277
marinersjk00 <kuschnerj@gmail.com>
2022-03-04 12:42:35 -0800

    Modified player classes

--- Player.cpp
+++ Player.cpp
@@ -4,92 +4,31 @@ class Player{
     
     private:
         int cost;
-        int numPoints, numAssists, numReb, numBlock, numSteal, numTO;
+        double points_scored;
     protected:
         String name, position;
         
-    Player(int cost, int pts, ints ats, int reb, int blk, int stl, int to, String name, String pos){
+    Player(int cost, double pts){
         this->cost = cost;
-        this->numPoints = pts;
-        this->numAssists = ats;
-        this->numReb = reb;
-        this->numBlock = blk;
-        this->numSteal = stl;
-        this->name = name;
-        this->position = pos;
+        this->points_scored = pts;
     }
     
     void setCost(int cost){
         this->cost = cost;
     }
     
-    void setPoints(int numPoints){
-        this->numPoints = numPoints;
-    }
-    
-    void setAssists(int numAssists){
-        this->numAssists = numAssists;
-    }
-    
-    void setReb(int numReb){
-        this->numReb = numReb;
-    }
-    
-    void setBlocks(int numBlock){
-        this->numBlock = numBlock;
-    }
-    
-    void setSteals(int numSteals){
-        this->numSteal= = numSteals;
-    }
-    
-    void setTO(int numTO){
-        this->numTO = numTO;
-    }
-    
-    void setName(String name){
-        this->name = name;
-    }
-    
-    void setPosition(String pos){
-        this->position = pos;
+    void setPoints(double numPoints){
+        this->points_scored = numPoints;
     }
     
     int getCost(){
         return this->cost;
     }
     
-    int getPts(){
-        return this->numPoints;
+    double getPts(){
+        return this->points_scored;
     }
     
-    int getAssists(){
-        return this->numAssists;
-    }
-    
-    int getReb(){
-        return this->numReb;
-    }
-    
-    int getBlock(){
-        return this->numBlock;
-    }
-    
-    int getSteal(){
-        return this->numSteals;
-    }
-    
-    int getTO(){
-        return this->numTO;
-    }
-    
-    String getName(){
-        return this->name;
-    }
-    
-    String getPosition(){
-        return this->position;
-    }
     
     float calculatePoints(){
         float pointsScored = 0.0;
@@ -100,7 +39,7 @@ class Player{
         pointsScored -= this->numTO;
         
         return pointsScored;
-    }
+  }
     
     
 }
--- Player.h
+++ Player.h
@@ -2,58 +2,18 @@ class Player{
     
     private:
         int cost;
-        int numPoints, numAssists, numReb, numBlock, numSteal, numTO;
+        double points_scored;
     protected:
         String name, position;
         
-    Player(int cost, int pts, ints ats, int reb, int blk, int stl, int to, String name, String pos){
+    Player(int cost, int pts){
         this->cost = cost;
-        this->numPoints = pts;
-        this->numAssists = ats;
-        this->numReb = reb;
-        this->numBlock = blk;
-        this->numSteal = stl;
-        this->name = name;
-        this->position = pos;
+        this->points_scored = pts;
     }
     
     void setCost(int cost){}
     
-    void setPoints(int numPoints){}
-    
-    void setAssists(int numAssists){}
-    
-    void setReb(int numReb){}
-    
-    void setBlocks(int numBlock){}
-    
-    void setSteals(int numSteals){}
-    
-    void setTO(int numTO){}
-    
-    void setName(String name){}
-    
-    void setPosition(String pos){}
-    
-    int getCost(){}
-    
-    int getPts(){}
-    
-    int getAssists(){}
-    
-    int getReb(){}
-    
-    int getBlock(){ }
-    
-    
-    int getTO(){ }
-    
-    String getName(){}
-    
-    String getPosition(){}
-    
-    float calculatePoints(){  }
-    
+    void setPoints(double points){}   
     
 }
 
