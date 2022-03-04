e7adb484512cef90c62fb43954eeae371f451101
marinersjk00 <kuschnerj@gmail.com>
2022-03-04 13:03:14 -0800

    Adding center.h

---
+++ center.h
@@ -0,0 +1,19 @@
+#ifndef __CENTER_HPP__
+#define __CENTER_HPP__
+
+#include "Player.h"
+#include <string>
+
+class Center : public Player {
+public:
+    Center(std::string name, int cost, double pts ) {
+        this->name = name;
+        this->cost = cost;
+        this->pts = pts;
+        this->position = "C";
+    }
+    
+};
+
+#endif //__CENTER_HPP__
+
