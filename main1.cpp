d59194624673ff6821bb7dff14ac52d731094d0a
noshotbigjuicersbilal151 <bnasi004@ucr.edu>
2022-03-06 11:25:01 -0800

    how to run python from cpp

---
+++ main1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+int main()
+{
+	//string filename = "/home/csmajs/bnasi004/final-project-jkusc002-bnasi004-cchua032-aoce003";
+	//string command = "python3 ";
+	//command += filename;
+	system("python3 /home/csmajs/bnasi004/final-project-jkusc002-bnasi004-cchua032-aoce003/hello.py");
+}
