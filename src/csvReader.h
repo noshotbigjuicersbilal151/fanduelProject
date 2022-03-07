3f0862ce451280f0a647b92ac9bc9beda2cb9ca8
xTh3Unkn0wnx <aoceg003@ucr.edu>
2022-03-07 11:51:46 -0800

    Moved all header files into a src file and created new header files from exsisting functions

---
+++ src/csvReader.h
@@ -0,0 +1,41 @@
+#ifndef __CVS_READER_H__
+#define __CVS_READER_H__
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<vector<string>> csvReader(){
+    string fname = "..//bestPlayers.csv";
+
+    vector<vector<string>> output;
+    vector<string> row;
+    string line, word;
+
+    fstream file (fname, ios::in);
+    if(file.is_open())
+    {
+        while(getline(file, line))
+        {
+            row.clear();
+
+            stringstream str(line);
+            if (!line.empty()){
+                while(getline(str, word, ',')) /*separates by using commas as delimiters*/
+                    row.push_back(word);
+                output.push_back(row);
+            }
+        }
+    }
+
+
+    return output;
+
+}
+
+#endif //__CVS_READER_H__
