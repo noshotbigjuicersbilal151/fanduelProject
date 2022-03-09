73854f016606dda4af0959de515153a5bb396174
marinersjk00 <kuschnerj@gmail.com>
2022-03-09 00:23:39 -0800

    Created Team Class to house methods. More OOP

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "src/bestLineUpAlgorithm.h"
 #include "src/teamSorter.h"
 #include "src/valueSorter.h"
+#include "src/Team.h"
 
 using namespace std;
 
@@ -16,26 +17,14 @@ int main(){
 
     vector<vector<string>> content = csvReader("bestPlayers.csv"); //loads player info as strings
 
-    vector<Player> sorted = playerSorter(content);
-
-/*sorting first by value (points * 1000 / cost)*/
-    valueSorter(sorted);
-
-    vector<Player> bestLineup = bestLineUpAlgorithm(sorted);
-
-/*outputting team and tracking team points and team salary*/
-    double totalPoints = 0.0;
-    int teamSalary = 0;
-    teamSorter(bestLineup);
-
-    std::cout << "Best Possible FanDuel Lineup: " << std::endl << std::endl;
-    for(int i = 0; i < bestLineup.size(); i++){
-        Player temp = bestLineup[i];
-        totalPoints += temp.getPoints();
-        teamSalary += temp.getCost();
-        std::cout << temp.getPos() << " " << temp.getName() << " " << temp.getPoints() << " $" << temp.getCost() << " " << temp.calculateValue() << std::endl;
-    }
-    std::cout << std::endl << "Total Points: " << totalPoints << "     Team Salary: $" << teamSalary << std::endl;
+    Team fanduel = Team(content);
+	fanduel.getPositions();
+	fanduel.valueSorter();
+	fanduel.bestByValue();
+	fanduel.sortByPointsScored();
+	fanduel.spendRemainingCapOnPoints();
+	fanduel.sortByPosition();
+	fanduel.printBestLineup();
     return 0;
 
 }
