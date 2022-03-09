4b7daa8ba8c09e758e09ffab8284111b4741d81f
xTh3Unkn0wnx <aoceg003@ucr.edu>
2022-03-08 16:23:12 -0800

    The unit_test.cpp is modified so that the python code can beexecuted before testing

--- unit_test.cpp
+++ unit_test.cpp
@@ -15,13 +15,21 @@
 
 using namespace std;
 
-TEST(VectorTest, FirstPlayer) {
+TEST(FixedCSV, FirstPlayer) {
 vector<vector<string>> contents = csvReader("test.csv");
 vector<Player> sorted = playerSorter(contents);
 EXPECT_EQ(" James^ Harden", sorted[0].getName());
 }
 
+TEST(DynamicCSV, FirstPlayer) {
+vector<vector<string>> contents = csvReader("bestPlayers.csv");
+vector<Player> sorted = playerSorter(contents);
+EXPECT_EQ(" James^ Harden", sorted[0].getName());
+}
+
 int main(int argc, char **argv) {
+  cout << "Enter 2, 28, then 2018 (Feb, 28 2018) for test" << endl;
+  system("python3 ~/final-project-jkusc002-bnasi004-cchua032-aoce003/webscraper.py");
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
